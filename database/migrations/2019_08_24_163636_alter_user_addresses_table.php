f5d2de97bfd218ce232e5c11e9d1a13565386c3e
Lany <wlany@qq.com>
2019-08-25 00:41:36 +0800

    收货地址列表

---
+++ database/migrations/2019_08_24_163636_alter_user_addresses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUserAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_addresses', function (Blueprint $table) {
+            //
+            $table->string('zip', 50)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_addresses', function (Blueprint $table) {
+            //
+        });
+    }
+}
